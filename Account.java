44be8480580ec45832edf82fa916f0dd7515ec49
Thibault NGUYEN <thibault.nguyen@ecam-rennes.com>
2022-09-03 09:37:38 +0000

    add account variables & constructor

---
+++ Account.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Account{
+    //Class Variables
+    int balance;
+    int previousTransaction;
+    String customerName;
+    String customerID;
+
+    //Class constructor
+    Account(String cname, String cid){
+        customerName = cname;
+        customerID = cid;
+    }
+
+    
+}  
