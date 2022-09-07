818bdebb67bf8c01f965c221f66479874174bf54
Thibault NGUYEN <thibault.nguyen@ecam-rennes.com>
2022-09-07 16:03:24 +0000

    main method show menu app

---
+++ FirstBank.java
@@ -0,0 +1,9 @@
+
+public class FirstBank {
+
+    public static void main(String[] args) {
+
+        Account thibault = new Account("Thibault NGUYEN", "N0001");
+        thibault.showMenu();
+    }
+}
