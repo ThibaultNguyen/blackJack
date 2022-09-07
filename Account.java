fc7a3442f2250fcb596ac9efcb7801c746ccb4b9
Thibault NGUYEN <thibault.nguyen@ecam-rennes.com>
2022-09-07 16:23:43 +0000

    app functions deposing, withdrawing & checking previous transaction

--- Account.java
+++ Account.java
@@ -13,5 +13,34 @@ public class Account{
         customerID = cid;
     }
 
+    //Function for Deposing money
+    void deposit(int amount) {
+        if (amount != 0) {
+            balance = balance + amount;
+            previousTransaction = amount;
+        }
+    }
+
+    //Function for Withdrawing money
+    void withdraw(int amount) {
+        if (amount !=0) {
+            balance = balance - amount;
+            previousTransaction = -amount;
+        }
+    }
+
+    //Function showing the previous transaction
+    void getPreviousTransaction() {
+        if (previousTransaction > 0) {
+            System.out.println("Deposited: " + previousTransaction);
+        } else if (previousTransaction < 0) {
+            System.out.println("Withdrawn: " + Math.abs(previousTransaction));
+        } else {
+            System.out.println("No transaction occured");
+        }
+    }
+
+    
+
     
 }  
