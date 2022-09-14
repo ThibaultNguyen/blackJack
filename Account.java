f2f40b6fbbb0762ee676b3295b08e5f135addb20
Thibault NGUYEN <thibault.nguyen@ecam-rennes.com>
2022-09-14 15:26:53 +0000

    function app menu

--- Account.java
+++ Account.java
@@ -48,7 +48,76 @@ public class Account{
         System.out.println("After " + year + " years, your balance will be : " + newBalance);
     }
 
-    
+    //Function showing the main menu
+    void showMenu() {
+        char option = '\0';
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome, " + customerID + " ! ");
+        System.out.println("Your ID is : " + customerID);
+        System.out.println();
+        System.out.println("What would you like to do ?");
+        System.out.println();
+        System.out.println("A. Check your balance");
+        System.out.println("B. Make a deposit");
+        System.out.println("C. Make a withdrawal");
+        System.out.println("D. View the previous transaction");
+        System.out.println("E. Calculate interest");
+        System.out.println("F. Exit");
+
+        do{
+            System.out.println();
+            System.out.println("Enter an option : ");
+            char option1 = scanner.next().charAt(0);
+            option = Character.toUpperCase(option1);
+            System.out.println();
+
+            switch(option) {
+                //Case 'A' allows the user to check their account balance
+                case 'A' :
+                System.out.println("==========================");
+                System.out.println("Balance = €" + balance);
+                System.out.println("==========================");
+                System.out.println();
+                break;
+                //Case 'B' allows the user to make a deposit in their with a specific amount using the 'deposit' function
+                case 'B' :
+                System.out.println("Enter the amount to deposit");
+                int amountDeposited = scanner.nextInt();
+                deposit(amountDeposited);
+                System.out.println();
+                break;
+                //Case 'C' allows the user to make a wihtdrawal with a specific amount using the 'withdraw' function
+                case 'C' :
+                System.out.println("Enter the amount to withdraw");
+                int amountWithdraw = scanner.nextInt();
+                withdraw(amountWithdraw);
+                System.out.println();
+                break;
+                //Case 'D' allows the user to check the previous transaction using the 'getPreviousTransaction' function
+                case 'D' :
+                System.out.println("==========================");
+                getPreviousTransaction();
+                System.out.println("==========================");
+                System.out.println();
+                break;
+                //Case 'E' allows the user to calculate the interest after a specified number of year
+                case 'E' :
+                System.out.println("Enter 'how many years' you want to estimate your interest in : ");
+                int years = scanner.nextInt();
+                calculateInterest(years);
+                break;
+                //Case 'F' the user exits from their account
+                case 'F' :
+                System.out.println("==========================");
+                break;
+                //The default case let the user know that they entered an invalid character and how to enter a valid one
+                defaut :
+                System.out.println("Error : invalid option. Please enter A, B, C, D, E or access services. ");
+                break;
+            }
+        } while(option != 'F');
+        System.out.println("Thank you for your trust !");
+    }
 
     
 }  
