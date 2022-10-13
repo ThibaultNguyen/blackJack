efe37a57cfda6a67047b5d6da037124f224e2d96
Thibault NGUYEN <thibault.nguyen@ecam-rennes.com>
2022-10-13 18:38:02 +0000

    GameRunner class created

---
+++ GameRunner.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner
+
+public class GameRunner {
+
+    public static void main(String[] args) {
+
+        //iniatialize
+        Scanner sc = new Scanner (System.in);
+        Deck theDeck = new Deck(1, true);
+    }
+}
